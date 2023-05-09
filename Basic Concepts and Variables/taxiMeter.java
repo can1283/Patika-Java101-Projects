c259fc1f2dcee1a586d3170bafeb3368cd299e37
Nurican Kaşıkcı <112473835+can1283@users.noreply.github.com>
2023-05-09 12:26:53 +0300

    Update taxiMeter.java

--- Basic Concepts and Variables/taxiMeter.java
+++ Basic Concepts and Variables/taxiMeter.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 /*
-    1 -> Taximeter costs $2.20 per kilometer.
-    2 -> The minimum amount to be paid is $20. $20 will be charged for fees below $20.
-    3 -> Taximeter opening fee is 10$.
+    Task:
+    Taximeter costs $2.20 per kilometer.
+    The minimum amount to be paid is $20. $20 will be charged for fees below $20.
+    Taximeter opening fee is 10$.
  */
 public class TaxiMeter {
     public static void main(String[] args) {
