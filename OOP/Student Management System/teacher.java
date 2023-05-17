2b2e8f78a4d44b34e670d48f8b002595ca1cf3af
Nurican Kaşıkcı <112473835+can1283@users.noreply.github.com>
2023-05-17 12:20:51 +0300

    Create teacher.java

---
+++ OOP/Student Management System/teacher.java
@@ -0,0 +1,14 @@
+package StudentManagementSystem;
+
+class Teacher {
+    String name;
+    String mpno;
+    String branch;
+
+    Teacher(String name, String mpno, String branch) {
+        this.name = name;
+        this.mpno = mpno;
+        this.branch = branch;
+
+    }
+}
