1050e51c873e2669077ac0697fae29d2101761b7
Nurican Kaşıkcı <112473835+can1283@users.noreply.github.com>
2023-05-17 13:42:36 +0300

    Create main.java

---
+++ OOP/Salary Calculation/main.java
@@ -0,0 +1,10 @@
+package Salary;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create an example employee
+        Employee e1 = new Employee("Kemal", 2000, 45, 1985);
+        // Print the employee's information and calculations
+        System.out.println(e1.toPrint());
+    }
+}
